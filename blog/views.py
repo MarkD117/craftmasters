855a702263edadc537de7aa6f39f32dbbdf401c9
Mark Dunne <Markdunne117@gmail.com>
2023-11-10 20:06:00 +0000

    Render categories as dropdown items in nav menu

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic, View
-
+from .models import Category
 
 class LandingPage(View):
+    # Specifying the template name
+    template_name = 'index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        categories = Category.objects.all()
+        context = {'categories': categories}
+        return render(request, self.template_name, context)
