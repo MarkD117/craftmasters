60c1a60ac9a520f7b0448d6d036b7a6e969690f7
Mark Dunne <Markdunne117@gmail.com>
2023-11-13 11:37:20 +0000

    Create view for project list

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,16 @@ class LandingPage(View):
     template_name = 'index.html'
 
     def get(self, request, *args, **kwargs):
+        # getting all categories
         categories = Category.objects.all()
+        # passing categories as context to the template
         context = {'categories': categories}
         return render(request, self.template_name, context)
+
+class ProjectList(generic.ListView):
+    model = Project
+    # queryset contains projects that have been published in descending order
+    queryset = Project.objects.filter(status=1).order_by('-created_on')
+    template_name = 'projects.html'
+    # if there are more than 6 projects, page navigation will be added
+    paginate_by = 6
