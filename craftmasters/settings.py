2fd132ee022f9e95ca5fbac4cede9b65764b85b7
Mark Dunne <Markdunne117@gmail.com>
2023-11-23 00:21:37 +0000

    Add message functionality

--- craftmasters/settings.py
+++ craftmasters/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -54,16 +55,25 @@ INSTALLED_APPS = [
     'blog',
 ]
 
-# site id set to 1 so django can handle
+# Site id set to 1 so django can handle
 # multiple sites from one database
 SITE_ID = 1
 
-# redirecting to home page on login/logout
+# Redirecting to home page on login/logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # User registration allows email field to be empty
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+# Assigning message tags to different bootstrap classes
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Ensure crispy library uses bootstrap4 classes for formatting
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
